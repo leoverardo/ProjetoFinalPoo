8a7731ce2c63bf1f94d1159999f1ff794484c9cf
leoverardo <leonardomoura080904@gmail.com>
2025-04-09 21:29:37 -0300

    Atualização

--- JavaApplication105/src/Model/Mercado.java
+++ JavaApplication105/src/Model/Mercado.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication105;
+package Model;
 
 /**
  *
  * @author aluno.saolucas
  */
-public class JavaApplication105 {
+public class Mercado {
 
     /**
      * @param args the command line arguments
