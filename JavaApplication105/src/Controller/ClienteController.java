8b7b2461e75094373bbd99983b143afad3741508
leoverardo <leonardomoura080904@gmail.com>
2025-04-11 19:49:08 -0300

    atualização da controller cliente

--- JavaApplication105/src/Controller/ClienteController.java
+++ JavaApplication105/src/Controller/ClienteController.java
@@ -1,9 +1,14 @@
 package Controller;
 
 import Model.Clientes;
+import Model.Produtos;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 
@@ -59,5 +64,100 @@ public class ClienteController {
       gerenciador.fecharConexao(comando); 
     }
     return false;      
+  }
+    
+    public List<Clientes> listarCliente() {
+    String sql = "SELECT * FROM clientes";
+    
+    GerenciadorConexao gerenciador = new GerenciadorConexao();
+    PreparedStatement comando = null;
+    ResultSet resultado = null;
+    
+    List<Clientes> listaClientes = new ArrayList<>();
+    
+    try {
+      comando = gerenciador.prepararComando(sql);
+
+      resultado = comando.executeQuery();
+
+      
+      while (resultado.next()) {
+
+        Clientes clientes = new Clientes();
+
+        clientes.setIdCliente(resultado.getInt("idProduto"));
+        clientes.setNome(resultado.getString("nome"));
+        clientes.setEmail(resultado.getString("email"));
+        clientes.setCpf(resultado.getString("cpf"));
+
+        listaClientes.add(clientes);
+      }
+
+    } catch (SQLException ex) {
+      Logger.getLogger(ProdutoController.class.getName()).log(
+              Level.SEVERE, null, ex);
+    } finally {
+      gerenciador.fecharConexao(comando, resultado);
+    }
+
+    return listaClientes;
+  }
+    
+    public Clientes buscarPorId(int idCliente) {
+    String sql = "SELECT * FROM clientes "
+            + " WHERE idCliente = ? ";
+
+    GerenciadorConexao gerenciador = new GerenciadorConexao();
+    PreparedStatement comando = null;
+    ResultSet resultado = null;
+
+    Clientes clientes = new Clientes();
+
+    try {
+      comando = gerenciador.prepararComando(sql);
+
+      comando.setInt(1, idCliente);
+
+      resultado = comando.executeQuery();
+
+      if (resultado.next()) {
+
+        clientes.setIdCliente(resultado.getInt("idProduto"));
+        clientes.setNome(resultado.getString("nome"));
+        clientes.setEmail(resultado.getString("email"));
+        clientes.setCpf(resultado.getString("cpf"));
+      }
+
+    } catch (SQLException ex) {
+      Logger.getLogger(ProdutoController.class.getName()).log(
+              Level.SEVERE, null, ex);
+    } finally {
+      gerenciador.fecharConexao(comando, resultado);
+    }
+    return clientes;
+  }
+    
+    
+    public boolean deletarCliente(int idCliente) {
+    String sql = "DELETE FROM clientes "
+               + "WHERE idCliente = ?";
+
+    GerenciadorConexao gerenciador = new GerenciadorConexao();
+    PreparedStatement comando = null;
+
+    try {
+      comando = gerenciador.prepararComando(sql);
+      comando.setInt(1, idCliente);
+
+      comando.executeUpdate();
+
+      return true;
+    } catch (SQLException ex) {
+      JOptionPane.showMessageDialog(null, "Erro ao excluir: "
+              + ex);
+    } finally {
+      gerenciador.fecharConexao(comando);
+    }
+    return false;
   }
 }
