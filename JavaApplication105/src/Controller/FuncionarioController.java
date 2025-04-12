e9cdb1a0a9c41f97204ad74b776b7a3c898a71cd
leoverardo <leonardomoura080904@gmail.com>
2025-04-11 21:54:16 -0300

    FrAlterarFuncionario

--- JavaApplication105/src/Controller/FuncionarioController.java
+++ JavaApplication105/src/Controller/FuncionarioController.java
@@ -1,9 +1,14 @@
 package Controller;
 
+
 import Model.Funcionario;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 
@@ -59,5 +64,144 @@ public class FuncionarioController {
       gerenciador.fecharConexao(comando); 
     }
     return false;      
+  }
+    
+    public boolean alterarFuncionario(Funcionario func) {
+    String sql = "UPDATE funcionarios SET nome = ?, "
+            + " email = ?";
+
+    if (func.getSenha() != null) {
+      sql = sql + " , senha = ? ";
+    }
+
+    sql = sql + " , cpf = ?, cargo = ? WHERE idFuncionario = ?";
+
+    GerenciadorConexao gerenciador = new GerenciadorConexao();
+    PreparedStatement comando = null;
+
+    try {
+      comando = gerenciador.prepararComando(sql);
+
+      comando.setString(1, func.getNome());
+      comando.setString(2, func.getEmail());
+
+      int numCampo = 3;
+      
+      if (func.getSenha() != null) {
+        comando.setString(numCampo, func.getSenha()); 
+        numCampo++;
+      }
+      
+      comando.setString(numCampo, func.getCpf());
+      numCampo++;
+      comando.setString(numCampo, func.getCargo());
+      comando.setInt(numCampo, func.getIdFuncionario());
+
+      comando.executeUpdate();
+
+      return true;
+    } catch (SQLException ex) {
+      JOptionPane.showMessageDialog(null, "Erro ao atualizar: " + ex);
+    } finally {
+      gerenciador.fecharConexao(comando);
+    }
+    return false;
+  }
+    
+    public List<Funcionario> listarFuncionario() {
+    String sql = "SELECT * FROM funcionarios";
+    
+    GerenciadorConexao gerenciador = new GerenciadorConexao();
+    PreparedStatement comando = null;
+    ResultSet resultado = null;
+    
+    List<Funcionario> listaFuncionarios= new ArrayList<>();
+    
+    try {
+      comando = gerenciador.prepararComando(sql);
+
+      resultado = comando.executeQuery();
+
+      
+      while (resultado.next()) {
+
+        Funcionario funcionario = new Funcionario();
+
+        funcionario.setIdFuncionario(resultado.getInt("idFuncionario"));
+        funcionario.setNome(resultado.getString("nome"));
+        funcionario.setEmail(resultado.getString("email"));
+        funcionario.setCpf(resultado.getString("cpf"));
+        funcionario.setCargo(resultado.getString("cargo"));
+
+        listaFuncionarios.add(funcionario);
+      }
+
+    } catch (SQLException ex) {
+      Logger.getLogger(ProdutoController.class.getName()).log(
+              Level.SEVERE, null, ex);
+    } finally {
+      gerenciador.fecharConexao(comando, resultado);
+    }
+
+    return listaFuncionarios;
+  }
+    
+    public Funcionario buscarPorId(int idFuncionario) {
+    String sql = "SELECT * FROM funcionarios "
+            + " WHERE idFuncionario = ? ";
+
+    GerenciadorConexao gerenciador = new GerenciadorConexao();
+    PreparedStatement comando = null;
+    ResultSet resultado = null;
+
+    Funcionario funcionario = new Funcionario();
+
+    try {
+      comando = gerenciador.prepararComando(sql);
+
+      comando.setInt(1, idFuncionario);
+
+      resultado = comando.executeQuery();
+
+      if (resultado.next()) {
+
+        funcionario.setIdFuncionario(resultado.getInt("idFuncionario"));
+        funcionario.setNome(resultado.getString("nome"));
+        funcionario.setEmail(resultado.getString("email"));
+        funcionario.setCpf(resultado.getString("cpf"));
+        funcionario.setCpf(resultado.getString("cargo"));
+      }
+
+    } catch (SQLException ex) {
+      Logger.getLogger(ProdutoController.class.getName()).log(
+              Level.SEVERE, null, ex);
+    } finally {
+      gerenciador.fecharConexao(comando, resultado);
+    }
+    return funcionario;
+  }
+    
+    
+    public boolean deletarFuncionario(int idFuncionario) {
+    String sql = "DELETE FROM funcionarios "
+               + "WHERE idFuncionario = ?";
+
+    GerenciadorConexao gerenciador = new GerenciadorConexao();
+    PreparedStatement comando = null;
+
+    try {
+      comando = gerenciador.prepararComando(sql);
+      comando.setInt(1, idFuncionario);
+
+      comando.executeUpdate();
+
+      return true;
+    } catch (SQLException ex) {
+      JOptionPane.showMessageDialog(null, "Erro ao excluir: "
+              + ex);
+    } finally {
+      gerenciador.fecharConexao(comando);
+    }
+    return false;
   }
 }
